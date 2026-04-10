3dd329775063a948590c26fd6bb887454937b6f4
meagybeezy <meaganbezin@gmail.com>
2026-04-10 16:59:25 -0700

    debug startup crash

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-from app import app
+print("RUN.PY STARTING")
+
+try:
+    from app import app
+    print("APP IMPORTED SUCCESSFULLY")
+except Exception as e:
+    print("ERROR IMPORTING APP:")
+    print(e)
+    raise
+
 from waitress import serve
+
+print("STARTING SERVER")
 
 serve(app, host='0.0.0.0', port=10000)
