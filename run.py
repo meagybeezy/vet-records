0cb62a29bb2680569bc0e8ba5a3094262a07c985
meagybeezy <meaganbezin@gmail.com>
2026-04-10 17:42:33 -0700

    fixed run.py location

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
-print("RUN.PY STARTING")
+import os
+from app import app
 
-try:
-    from app import app
-    print("APP IMPORTED SUCCESSFULLY")
-except Exception as e:
-    print("ERROR IMPORTING APP:")
-    print(e)
-    raise
-
-from waitress import serve
-
-print("STARTING SERVER")
-
-serve(app, host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
