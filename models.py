ffcd92eafa6d1ab70e5e33128d9fb731058b2df4
meagybeezy <meaganbezin@gmail.com>
2026-04-10 17:31:55 -0700

    add init_db function

--- models.py
+++ models.py
@@ -89,4 +89,9 @@ if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://")
 
 engine = create_engine(DATABASE_URL)
+
 Session = sessionmaker(bind=engine)
+session = Session()
+
+def init_db():
+    Base.metadata.create_all(engine)
